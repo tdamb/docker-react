584cccc390c78ca9d000eb84fc411b07662294ee
tdamb <tomas.dambrauskass@gmail.com>
2022-10-12 12:07:44 +0300

    delete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Trek procaliber 9.7
+          Trek procaliber 9.7 
         </p>
         <a
           className="App-link"
